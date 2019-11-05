25eac7e2c75e198198b23c2bd1e062f01eeff3f0
jieyingjian <392502304@qq.com>
2019-11-05 18:31:14 +0800

    显示默认额度

--- alliance-core/src/main/java/com/jfeat/am/module/alliance/api/AllianceEndpoint.java
+++ alliance-core/src/main/java/com/jfeat/am/module/alliance/api/AllianceEndpoint.java
@@ -292,9 +292,13 @@ public class AllianceEndpoint {
         Date end = calculationEndTime();
         for(AllianceRecord allianceRecord: alliancePage){
             allianceRecord.setCutOffTime(end);
-//            if(allianceRecord.getBalance()==null||allianceRecord.getBalance().compareTo(new BigDecimal(0))==0){
-//
-//            }
+            if(allianceRecord.getBalance()==null||allianceRecord.getBalance().compareTo(new BigDecimal(0))==0){
+                if(allianceRecord.getAllianceType()==Alliance.ALLIANCE_TYPE_BONUS){
+                    allianceRecord.setBalance(new BigDecimal(configFieldService.getFieldFloat(AllianceFields.ALLIANCE_FIELD_BONUS_ALLIANCE)));
+                }else {
+                    allianceRecord.setBalance(new BigDecimal(configFieldService.getFieldFloat(AllianceFields.ALLIANCE_FIELD_COMMON_ALLIANCE)));
+                }
+            }
         }
         page.setRecords(alliancePage);
         return SuccessTip.create(page);
