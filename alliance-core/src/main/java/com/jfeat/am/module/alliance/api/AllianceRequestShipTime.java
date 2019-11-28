7d2ce91556600aa92a3642ea4b7d4f5430d133a1
jieyingjian <392502304@qq.com>
2019-11-28 14:25:57 +0800

    添加action修改时间

---
+++ alliance-core/src/main/java/com/jfeat/am/module/alliance/api/AllianceRequestShipTime.java
@@ -0,0 +1,15 @@
+package com.jfeat.am.module.alliance.api;
+
+import java.util.Date;
+
+public class AllianceRequestShipTime {
+    private Date allianceShipTime;
+
+    public Date getAllianceShipTime() {
+        return allianceShipTime;
+    }
+
+    public void setAllianceShipTime(Date allianceShipTime) {
+        this.allianceShipTime = allianceShipTime;
+    }
+}
--- bonus/src/main/java/com/jfeat/am/module/bonus/api/AllianceReconciliationEndpoint.java
+++ bonus/src/main/java/com/jfeat/am/module/bonus/api/AllianceReconciliationEndpoint.java
@@ -1,8 +1,11 @@
 package com.jfeat.am.module.bonus.api;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.jfeat.am.common.annotation.Permission;
 import com.jfeat.am.module.bonus.definition.BonusPermission;
 import com.jfeat.am.module.bonus.services.domain.dao.QueryBonusDao;
+import com.jfeat.am.module.bonus.services.domain.model.AllianceReconciliation;
+import com.jfeat.am.module.bonus.services.domain.model.ProductSalesRecord;
 import com.jfeat.am.module.bonus.services.domain.service.BonusService;
 import com.jfeat.crud.base.tips.SuccessTip;
 import com.jfeat.crud.base.tips.Tip;
@@ -26,15 +29,23 @@ public class AllianceReconciliationEndpoint {
     @Permission(BonusPermission.BONUS_VIEW)
     public Tip sales(@RequestParam(name = "pageNum", required = false, defaultValue = "1") Integer pageNum,
                      @RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer pageSize,
-                     @RequestParam(name = "search", required = false) String search){
-
-        return SuccessTip.create(queryBonusDao.querySales(pageNum,pageSize,search));
+                     @RequestParam(name = "search", required = false) String search) {
+        Page<ProductSalesRecord> page=new Page<>();
+        page.setRecords(queryBonusDao.querySales(pageNum, pageSize, search));
+        page.setSize(pageSize);
+        page.setCurrent(pageNum);
+        return SuccessTip.create(page);
     }
+
     @GetMapping("/allianceReconciliation")
     @Permission(BonusPermission.BONUS_DIVIDEND)
     public Tip allianceReconciliation(@RequestParam(name = "pageNum", required = false, defaultValue = "1") Integer pageNum,
-                     @RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer pageSize,
-                                      @RequestParam(name = "search", required = false) String search){
-        return SuccessTip.create(bonusService.getAllianceReconciliation(pageNum,pageSize,search));
+                                      @RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer pageSize,
+                                      @RequestParam(name = "search", required = false) String search) {
+        Page<AllianceReconciliation> page=new Page<>();
+        page.setRecords(bonusService.getAllianceReconciliation(pageNum, pageSize, search));
+        page.setSize(pageSize);
+        page.setCurrent(pageNum);
+        return SuccessTip.create(page);
     }
 }
