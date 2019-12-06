0812aa94d033d00f051e21fabb8e4a55daba1df1
yejiacheng <yjc@example.com>
2019-12-06 11:37:32 +0800

    资金明细 操作金额 搜索

--- alliance-core/src/main/java/com/jfeat/am/module/alliance/api/walletHistoryEndpoint.java
+++ alliance-core/src/main/java/com/jfeat/am/module/alliance/api/walletHistoryEndpoint.java
@@ -14,6 +14,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -29,11 +30,16 @@ public class walletHistoryEndpoint {
                            @RequestParam(name = "pageNum", required = false, defaultValue = "1") Integer pageNum,
                            @RequestParam(name = "pageSize", required = false, defaultValue = "10") Integer pageSize,
                            @RequestParam(name = "search", required = false) String search,
+                           @RequestParam(name = "searchMoney", required = false) BigDecimal searchMoney[],
                            @RequestParam(name = "type", required = false) String type
                             ) {
         page.setCurrent(pageNum);
         page.setSize(pageSize);
-        List<WalletHistoryRecord> walletHistoryRecord=queryWalletHistoryDao.findWalletHistoryPage(page,search,type);
+        BigDecimal leftMoney = searchMoney!=null? (searchMoney.length > 0?searchMoney[0]:null) : null;
+        BigDecimal rightMoney = searchMoney!=null ? (searchMoney.length==2?searchMoney[1]:(searchMoney.length==1?searchMoney[0]:null)) : null;
+
+
+        List<WalletHistoryRecord> walletHistoryRecord=queryWalletHistoryDao.findWalletHistoryPage(page,search,type,leftMoney,rightMoney);
         for (WalletHistoryRecord walletHistory:walletHistoryRecord) {
             if(walletHistory.getType()!=null && walletHistory.getType().equals("WITHDRAW")){
                 walletHistory.setOwnBlance(walletHistory.getBalance());
--- alliance-core/src/main/java/com/jfeat/am/module/alliance/services/domain/dao/QueryWalletHistoryDao.java
+++ alliance-core/src/main/java/com/jfeat/am/module/alliance/services/domain/dao/QueryWalletHistoryDao.java
@@ -6,6 +6,7 @@ import com.jfeat.am.module.alliance.services.domain.model.WalletHistoryRecord;
 import com.jfeat.am.module.alliance.services.gen.persistence.model.WalletHistory;
 import org.apache.ibatis.annotations.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -15,7 +16,10 @@ public interface QueryWalletHistoryDao extends BaseMapper<WalletHistory> {
 
     List<WalletHistoryRecord> findWalletHistoryPage(Page<WalletHistoryRecord> page,
                                              @Param("search") String search,
-                                                    @Param("type") String type);
+                                                    @Param("type") String type,
+                                                    @Param("leftMoney") BigDecimal leftMoney,
+                                                    @Param("rightMoney") BigDecimal rightMoney
+                                                    );
 
 
 }
