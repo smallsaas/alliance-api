5a5ed4251769e42e71848319fd004bf7f9ad522e
yejiacheng <yjc@example.com>
2020-01-02 10:45:08 +0800

    @EnableScheduling 开启自动功能

--- alliance-rpc/src/main/java/com/jfeat/util/BonusHandle.java
+++ alliance-rpc/src/main/java/com/jfeat/util/BonusHandle.java
@@ -11,6 +11,7 @@ import com.jfeat.am.module.bonus.services.domain.model.AllianceReconciliation;
 import com.jfeat.am.module.bonus.services.domain.service.BonusService;
 import com.jfeat.am.module.bonus.services.domain.service.SettlementCenterService;
 import com.jfeat.am.module.config.services.service.ConfigFieldService;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
@@ -20,6 +21,7 @@ import java.util.List;
 
 //计算分红
 @Component
+@EnableScheduling
 public class BonusHandle {
     @Resource
     QueryAllianceDao queryAllianceDao;
@@ -35,36 +37,8 @@ public class BonusHandle {
     @Resource
     SettlementCenterService settlementCenterService;
 
-    @Scheduled(cron = "0 0 1 1 * ?")//凌晨一点？
+    @Scheduled(cron = "0 0 1 1 * ?")//每月1号1点
     public void task() {
-//        BigDecimal config = new BigDecimal(configFieldService.getFieldFloat(AllianceFields.ALLIANCE_FIELD_WITHDRAWAL_CONDITIONS));
-//        List<AllianceReconciliation> allianceReconciliations = queryBonusDao.queryReInformation(null);
-//        for(AllianceReconciliation r:allianceReconciliations){
-//            BigDecimal add = queryBonusDao.getCommissionOrderTotalLastMonth(r.getUserId());
-//            if(add==null){
-//                add=new BigDecimal(0.00);
-//            }
-//            int condition = add.subtract(config).compareTo(new BigDecimal(0.00));
-//            if(condition>=0){
-//                OwnerBalance theownerBalance =  new OwnerBalance();
-//                theownerBalance.setUserId(r.getUserId());
-//                OwnerBalance ownerBalance = queryOwnerBalanceDao.selectOne(theownerBalance);
-//                if(ownerBalance==null){
-//                    ownerBalance=new OwnerBalance();
-//                    ownerBalance.setUserId(r.getUserId());
-//                    ownerBalance.setBalance(add);
-//
-//                    queryOwnerBalanceDao.insert(ownerBalance);
-//                }else {
-//                    BigDecimal bonus_balance = ownerBalance.getBalance();
-//                    if(bonus_balance!=null){
-//                        BigDecimal add1 = bonus_balance.add(add);
-//                        ownerBalance.setBalance(add1);
-//                    }
-//                    queryOwnerBalanceDao.updateById(ownerBalance);
-//                }
-//            }
-//        }
         List<Long> ids = queryBonusDao.queryOrderId();
         if(ids!=null&&ids.size()>0){
             for(Long id:ids){
@@ -72,8 +46,9 @@ public class BonusHandle {
             }
         }
     }
+
     //凌晨
-    @Scheduled(cron = "0 0 0 * * ?")
+    @Scheduled(cron = "0 0 0 * * ?")//每天0点0分0秒
     public void task2() {
         List<Alliance> alliances = queryAllianceDao.selectList(null);
         if (alliances != null && alliances.size() > 0) {
@@ -91,5 +66,15 @@ public class BonusHandle {
 
 
     }
+   // @Scheduled(cron = "0/5 * * * * ?")//秒分时日月周  0/5 从0秒开始 每5秒执行一次   0-5 区间 0,1,2,3,4,5
+  /*  public void task3() {
+        List<Long> ids = queryBonusDao.queryOrderId();
+        if(ids!=null&&ids.size()>0){
+            for(Long id:ids){
+                System.out.println(id);
+                //settlementCenterService.settlementOrder(id);
+            }
+        }
+    }*/
 
 }
